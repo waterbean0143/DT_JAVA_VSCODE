dd703a498041e83dde01a580019d620868bce25f
Waterbean_DT <sbbae123@naver.com>
2022-09-08 12:32:50 +0900

    22.09.08 / 12:35 기준 일괄 최신화

--- src/ch09/BigIntegerEx.java
+++ src/ch09/BigIntegerEx.java
@@ -4,9 +4,9 @@ import java.math.*;
 
 class BigIntegerEx {
 	public static void main(String[] args) throws Exception {
-		BigInteger big =  BigInteger.ONE;
+		BigInteger big = BigInteger.ONE;
 
-		for(int i=1; i<100;i++) { // 1!부터 99!까지 출력
+		for (int i = 1; i < 100; i++) { // 1!부터 99!까지 출력
 			System.out.printf("%d!=%s%n", i, calcFactorial(i));
 			Thread.sleep(300); // 0.3초의 지연
 		}
@@ -17,9 +17,9 @@ class BigIntegerEx {
 	}
 
 	static BigInteger factorial(BigInteger n) {
-		if(n.equals(BigInteger.ZERO)) {
+		if (n.equals(BigInteger.ZERO)) {
 			return BigInteger.ONE;
-		} else {  // return n * factorial(n-1);
+		} else { // return n * factorial(n-1);
 			return n.multiply(factorial(n.subtract(BigInteger.ONE)));
 		}
 	}
